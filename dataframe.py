466e6fc70ed26f3a8cb63d2c0576a2c1d423c8bc
Wei Ting Lin <greg19941029@gmail.com>
2020-03-09 21:19:47 +0800

    new

--- dataframe.py
+++ dataframe.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
+import re
 
 fdc = {'FDC ALARM': 'FDC alarm'}
-down = {'DOWN': 'Tool Down'}
+down = {'DOWN': 'tool down'}
 pilot = {'PILOT': 'Tool/BKM pilot'}
 rule = [fdc, down, pilot]
 
@@ -28,6 +29,29 @@ if __name__ == '__main__':
                             isget = True
                             hold_purpose = key
                             hold_time = tmp
+                            if value == 'tool down':
+                                upper = upper[upper.Date < tmp.Date[0]]
+                                tmp = upper.tail(1).reset_index(drop=True)
+                                equip = re.split(r' ',comment['Comment'][i])
+                                tmp1 = 0
+                                for index,k in enumerate(equip):
+                                    if 'DOWN' in k.upper():
+                                        if index != 0:
+                                            if equip[index-1] == tmp.ix[0,'Equip'] or equip[index+1] == tmp.ix[0,'Equip']:
+                                                tmp1 = 1
+                                                hold_purpose = '當站' + hold_purpose
+                                                break
+                                        else:
+                                            if equip[index+1] == tmp.ix[0,'Equip']:
+                                                hold_purpose = '當站' + hold_purpose
+                                                tmp1 = 1
+                                                break
+                                if tmp1 == 0:
+                                    hold_purpose = '前站' + hold_purpose
+
+                                #if tmp.ix[0,'Equip'] == 'aassdd':
+                                #    hold_purpose = '當站' + hold_purpose
+
                             break
                     if isget: break
                 if isget: break    
